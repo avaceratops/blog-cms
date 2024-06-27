df0568002d1eb785bd13187267410de50830145c
ava <148492613+avaceratops@users.noreply.github.com>
2024-06-27 15:44:40 +0100

    Fix authReducer not setting headers correctly
    - Updated to use axios instance from api.js
    - Fixed header being set to action.payload rather than
    action.payload.token

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import api from '../utils/api';
 
 export default function authReducer(state, action) {
   switch (action.type) {
     case 'setToken':
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + action.payload;
+      api.defaults.headers.common['Authorization'] = 'Bearer ' + action.payload.token;
       localStorage.setItem('token', action.payload.token);
       localStorage.setItem('user', action.payload.user);
       return { ...state, token: action.payload.token, user: action.payload.user };
     case 'clearToken':
-      delete axios.defaults.headers.common['Authorization'];
+      delete api.defaults.headers.common['Authorization'];
       localStorage.removeItem('token');
       localStorage.removeItem('user');
       return { ...state, token: null, user: null };
