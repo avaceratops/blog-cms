eecdf15bc90e0aa18b887766c73adeee796f095c
ava <148492613+avaceratops@users.noreply.github.com>
2024-06-27 15:53:51 +0100

    Update main.jsx to use AuthProvider and Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/Root';
-import Post from './routes/Post';
-import Posts from './routes/Posts';
-import PageNotFound from './components/PageNotFound';
+import AuthProvider from './providers/AuthProvider';
+import Routes from './routes/Routes';
 import './index.scss';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: '/',
-        element: <Posts />,
-      },
-      {
-        path: '/post/:postId',
-        element: <Post />,
-      },
-      {
-        path: '/*',
-        element: <PageNotFound />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   </React.StrictMode>
 );
