8b37356dbf30e5bd14ba5f4e0b3d5db97c819def
ava <148492613+avaceratops@users.noreply.github.com>
2024-06-28 13:19:51 +0100

    Add hidden status to BlogPostStatus

--- src/components/BlogPostStatus.jsx
+++ src/components/BlogPostStatus.jsx
@@ -1,9 +1,12 @@
 import { DateTime } from 'luxon';
 import styles from '../styles/BlogPost.module.scss';
 
-export default function BlogPostStatus({ published }) {
+export default function BlogPostStatus({ published, hidden }) {
   const publishedTime = DateTime.fromISO(published);
 
+  if (hidden) {
+    return <p className={styles['status--hidden']}>Hidden</p>;
+  }
   if (DateTime.now() > publishedTime) {
     return <p className={styles['status--published']}>Published</p>;
   }
