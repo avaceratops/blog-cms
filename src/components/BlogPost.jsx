b470de2bee081cf105920c5ddd1b322942934488
ava <148492613+avaceratops@users.noreply.github.com>
2024-07-04 13:15:17 +0100

    Change BlogPost to use BlogPostForm
    The form is rendered when excerpt is false. Thus, it should only be
    displayed when viewing an individual post.

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
+import BlogPostForm from './BlogPostForm';
 import BlogPostMeta from './BlogPostMeta';
-import BlogPostMetaFull from './BlogPostMetaFull';
 import BlogPostTitle from './BlogPostTitle';
 import BlogPostMessage from './BlogPostMessage';
 import BlogPostStatus from './BlogPostStatus';
@@ -13,24 +13,27 @@ export default function BlogPost({ post, excerpt }) {
     setHidden(!hidden);
   }
 
-  return (
-    <article className="flex flex-col gap-1 xs:flex-row xs:gap-4">
-      {excerpt && (
+  if (excerpt) {
+    return (
+      <article className="flex flex-col gap-1 xs:flex-row xs:gap-4">
         <BlogPostMeta published={post.timestamps.published} username={post.author.username} />
-      )}
 
-      <section>
-        <BlogPostTitle title={post.title} />
-        {!excerpt && (
-          <BlogPostMetaFull published={post.timestamps.published} username={post.author.username} />
-        )}
-        <BlogPostMessage id={post._id} message={post.message} excerpt={excerpt} />
+        <section>
+          <BlogPostTitle title={post.title} />
+          <BlogPostMessage id={post._id} message={post.message} />
 
-        <div className="flex gap-1">
-          <BlogPostStatus published={post.timestamps.published} hidden={hidden} />
-          <HideButton id={post._id} hidden={hidden} toggleHidden={toggleHidden} />
-        </div>
-      </section>
+          <div className="flex gap-1">
+            <BlogPostStatus published={post.timestamps.published} hidden={hidden} />
+            <HideButton id={post._id} hidden={hidden} toggleHidden={toggleHidden} />
+          </div>
+        </section>
+      </article>
+    );
+  }
+
+  return (
+    <article className="flex w-full flex-col gap-1 xs:gap-4">
+      <BlogPostForm post={post} editing={true} />
     </article>
   );
 }
