0f65d84437b2b56d2bb92eb1f80b61dcd88b3f85
ava <148492613+avaceratops@users.noreply.github.com>
2024-07-04 13:14:16 +0100

    Change BlogPostMessage to only return excerpts

--- src/components/BlogPostMessage.jsx
+++ src/components/BlogPostMessage.jsx
@@ -2,15 +2,13 @@ import { Link } from 'react-router-dom';
 import { createBlogExcerpt } from '../utils/formatting';
 import styles from '../styles/BlogPost.module.scss';
 
-export default function BlogPostMessage({ id, message, excerpt }) {
+export default function BlogPostMessage({ id, message }) {
   return (
-    <p className={`${!excerpt ? 'whitespace-pre-line' : ''} max-w-prose text-lg`}>
-      {excerpt ? createBlogExcerpt(message, 50) : message}{' '}
-      {excerpt && (
-        <Link to={`/post/${id}`}>
-          <span className={styles.expand}>...edit</span>
-        </Link>
-      )}
+    <p>
+      {createBlogExcerpt(message, 50)}{' '}
+      <Link to={`/post/${id}`}>
+        <span className={styles.expand}>...edit</span>
+      </Link>
     </p>
   );
 }
