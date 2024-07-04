a5e04b4b76657222d7bc5cf2b33dcedb99494dbd
ava <148492613+avaceratops@users.noreply.github.com>
2024-07-04 13:13:26 +0100

    Add 'Add new post' link to Posts route

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import BlogPost from '../components/BlogPost';
 import useBlogPosts from '../hooks/useBlogPosts';
 
@@ -13,6 +14,14 @@ export default function Posts() {
 
   return (
     <section className="flex flex-col gap-12">
+      <Link
+        to="/post/new"
+        className="hover:bg-neutral-7 00 self-start rounded border border-violet-300 px-4 py-2
+          text-violet-300 hover:border-violet-400 hover:text-violet-400"
+      >
+        Add new post
+      </Link>
+
       {data.posts.map((post) => (
         <BlogPost key={post._id} post={post} excerpt={true} />
       ))}
