1bb0ee55a3852fe14baf743e6e3310452640e6fe
ava <148492613+avaceratops@users.noreply.github.com>
2024-07-04 12:51:48 +0100

    Add getLocalTime function to formatting utils
    Removes time zone data from ISO 8601 dates, which allows them to be used
    as the value for a datetime-local input.
    
    Time zones are handled automatically by luxon.

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -1,4 +1,15 @@
+import { DateTime } from 'luxon';
+
 export function createBlogExcerpt(text, maxWords) {
   const words = text.split(' ');
   return words.length > maxWords ? words.slice(0, maxWords).join(' ') : text;
+}
+
+export function getLocalTime(isoTime) {
+  return DateTime.fromISO(isoTime)
+    .set({
+      second: 0,
+      millisecond: 0,
+    })
+    .toISO({ includeOffset: false });
 }
